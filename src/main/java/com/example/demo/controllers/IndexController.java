ce94d655756056c526d66af27022f95c2eac95e6
Pedro Flores Cortes <pedro.flores@jarbrect.com.mx>
2021-05-29 02:51:37 -0500

    mi hola mundo con spring

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,5 +1,14 @@
 package com.example.demo.controllers;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class IndexController {
+	
+	@GetMapping({"/index","/","/home"})
+	public String index() {
+		return "index";
+	}
+	
 }
