bdb5a62ca6fd44c7318c1c96e25c480cb1e3ca45
Pedro Flores Cortes <pedro.flores@jarbrect.com.mx>
2021-07-03 01:58:10 -0500

    Curso de spring

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
 	@GetMapping({"/index","/","/home"})
-	public String index() {
+	public String index(Model model) {
+		
+		model.addAttribute("titulo", "Hola Spring framework");
 		return "index";
 	}
 	
